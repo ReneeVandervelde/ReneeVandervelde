13a3561defa6807f4a17708307c5eb5d6889ce26
Renee Vandervelde <Renee@ReneeVandervelde.com>
2025-01-04 17:25:57 -0600

    Publish Photo: Foggy Golden Gate

---
+++ src/staticMain/photography/photos/2021-10-17-foggy-golden-gate.inkui.kts
@@ -0,0 +1,15 @@
+include("base.inkui.part.kts")
+
+photo(
+    title = "Foggy Golden Gate",
+    full = "https://firebasestorage.googleapis.com/v0/b/reneevandervelde-d88ac.firebasestorage.app/o/20211017-_DSC0569-resized-2048.jpg?alt=media&token=683b9cd7-c4ef-4073-836f-a2f2c69cdd11",
+    thumbnail = "https://firebasestorage.googleapis.com/v0/b/reneevandervelde-d88ac.firebasestorage.app/o/20211017-_DSC0569-thumbnail-480.jpg?alt=media&token=61e05646-6622-42af-8aec-1e8d3526cfde",
+    alt = "Photo of the golden gate bridge on a foggy and rainy day, silhouetted by the trees in the foreground.",
+    description = """
+        I love to hike on a rainy day. There's something peaceful about 
+        walking along a trail in the rain. This was taken walking along 
+        Land's End trail. We completed the hike just as it began to rain.
+    """.trimIndent(),
+    taken = LocalDate(2021, 10, 17),
+    published = LocalDate(2025, 1, 4),
+)
