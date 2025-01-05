eb4151e6b33e5829824729ae8df50c363b8d79fe
Renee Vandervelde <Renee@ReneeVandervelde.com>
2025-01-05 16:06:16 -0600

    Publish photo: New Growth

---
+++ src/staticMain/photography/photos/2023-08-10-new-growth.inkui.kts
@@ -0,0 +1,14 @@
+include("base.inkui.part.kts")
+
+photo(
+    title = "New Growth",
+    full = "https://firebasestorage.googleapis.com/v0/b/reneevandervelde-d88ac.firebasestorage.app/o/20230810-_DSC1674-resized-2048.jpg?alt=media&token=aa0aea8a-7a26-4616-817b-ea242637c452",
+    thumbnail = "https://firebasestorage.googleapis.com/v0/b/reneevandervelde-d88ac.firebasestorage.app/o/20230810-_DSC1674-thumbnail-480.jpg?alt=media&token=8225dbb0-1362-4480-98be-6f791ecad087",
+    alt = "Photo of a log, covered in moss, with a mushroom growing out of it.",
+    description = """
+        Hiking through dense forest, finding pretty mushrooms.
+        Taken while camping in Lakewood, Wisconsin.
+    """.trimIndent(),
+    taken = LocalDate(2023, 8, 10),
+    published = LocalDate(2025, 1, 5),
+)
