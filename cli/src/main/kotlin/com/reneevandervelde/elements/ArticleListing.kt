0e386462662627ecc641378ad8596f31355e0a00
Renee Vandervelde <Renee@ReneeVandervelde.com>
2025-03-16 15:55:32 -0500

    Update style of publications index

--- cli/src/main/kotlin/com/reneevandervelde/elements/ArticleListing.kt
+++ cli/src/main/kotlin/com/reneevandervelde/elements/ArticleListing.kt
@@ -4,25 +4,31 @@ import com.reneevandervelde.rss.RssItem
 import ink.ui.render.statichtml.renderer.ElementRenderer
 import ink.ui.structures.elements.UiElement
 import ink.ui.structures.render.RenderResult
+import kotlinx.datetime.LocalDate
 import kotlinx.html.*
 
 data class ArticleListing(
     val title: String,
     val description: String,
-    val link: String
+    val link: String,
+    val published: LocalDate,
 ): UiElement.Static {
     object Renderer: ElementRenderer {
         override fun TagConsumer<*>.render(element: UiElement, parent: ElementRenderer): RenderResult {
             if (element !is ArticleListing) return RenderResult.Skipped
 
-            article {
-                a(href = element.link) {
-                    h2 {
-                        +element.title
+            article("article-index") {
+                header {
+                    a(href = element.link) {
+                        h2 {
+                            +element.title
+                        }
                     }
-                }
-                p {
-                    +element.description
+                    time {
+                        attributes["datetime"] = "${element.published}"
+                        attributes["pubdate"] = "pubdate"
+                        +element.published.toString()
+                    }
                 }
             }
 
@@ -33,5 +39,6 @@ data class ArticleListing(
 fun ArticleListing(rssItem: RssItem, baseUrl: String) = ArticleListing(
     title = rssItem.title,
     description = rssItem.description,
-    link = "$baseUrl/${rssItem.fileName}"
+    link = "$baseUrl/${rssItem.fileName}",
+    published = rssItem.published,
 )
