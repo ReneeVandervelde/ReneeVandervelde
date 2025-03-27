3752567072b12f8ff720638275254831e32141bc
Renee Vandervelde <Renee@ReneeVandervelde.com>
2025-03-27 11:09:17 -0500

    Publish bitkey article

---
+++ src/staticMain/publications/generically-typed/bitkey-inheritance-tech.inkui.kts
@@ -0,0 +1,10 @@
+include("base.inkui.part.kts")
+
+crossPost(
+    url = "https://bitkey.build/bitkeys-inheritance-feature-a-technical-deep-dive/",
+    title = "Bitkey’s inheritance feature: A technical deep dive",
+    description = """
+        Technical write-up on Bitkey's inheritance encryption and transfer mechanisms.
+    """.trimIndent(),
+    published = LocalDate(2025, 3, 27),
+)
